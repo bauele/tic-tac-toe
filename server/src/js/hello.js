2c9bb5c2adf48f6e223a7dfa5ebfc759ac2d7b93
bauele <bauele@users.noreply.github.com>
2022-12-22 15:17:44 -0500

    Introduce new directory structure with client and server folders
    Additionally, configuration files have been supplied for the server
    folder in preparation for adding backend logic. These configurations
    include Typescript, Jest, and Prettier.

---
+++ server/src/js/hello.js
@@ -0,0 +1,6 @@
+"use strict";
+let my_name;
+let age;
+my_name = "Kyle";
+age = 29;
+console.log(`Hello ${my_name}, you are ${age}!`);
---
+++ server/src/ts/hello.ts
@@ -0,0 +1,7 @@
+let my_name: string;
+let age: number;
+
+my_name = "Kyle";
+age = 29;
+
+console.log(`Hello ${my_name}, you are ${age}!`);
