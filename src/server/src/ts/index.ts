d61fb0dd7d680282064ef8c474e937e2aabc6d22
bauele <bauele@users.noreply.github.com>
2023-01-06 04:17:21 -0500

    Clean up server logic and add comments

--- src/server/src/ts/index.ts
+++ src/server/src/ts/index.ts
@@ -3,16 +3,16 @@ import { Server } from 'socket.io';
 
 import { Player, PlayerType, playerAIStrategy } from './player';
 
-import { gameModes, gameSettings } from './lib';
+import { gameSettings } from './lib';
 import serverConfig from './config.json';
 import { Session } from './session';
 import { Mark } from './enums';
 
-// Map connecting client socket ids to players
-const socketGameMap = new Map<string, Player[]>();
+//  Map connecting client socket ids to players
+const socketPlayerMap = new Map<string, Player[]>();
 
-// Map connecting game ids to GameInstance objects
-const sessionIdMap = new Map<Player, Session>();
+//  Map connecting players to sessions
+const playerSessionMap = new Map<Player, Session>();
 
 const io = new Server({
     cors: {
@@ -26,21 +26,21 @@ io.on('connection', (socket) => {
     socket.on('disconnect', (reason) => {
         console.log(`Socket disconnected for ${reason}`);
 
-        /*
-
-        // Delete player's game
-        const player = socketGameMap.get(socket.id);
-        if (player) {
-            if (gameIdMap.delete(player.gameId)) {
+        //  Delete player's game
+        const player = socketPlayerMap.get(socket.id);
+        if (player && player.length > 0) {
+            //  As long as the returned player array has at least one player in
+            //  it, that player can be used to delete the entire session
+            if (playerSessionMap.delete(player[0])) {
                 console.log(`Deleted ${socket.id}'s game`);
             } else {
                 console.log(`Error: failed to deleted ${socket.id}'s game`);
             }
 
-            // Delete player's socket
-            socketGameMap.delete(socket.id);
+            // Delete players
+            socketPlayerMap.delete(socket.id);
+            console.log(`Removed ${socket.id} from server`);
         }
-        */
     });
 
     socket.on('new-game', (gameConfig: gameSettings) => {
@@ -58,6 +58,9 @@ io.on('connection', (socket) => {
             eventListener: new EventEmitter(),
         };
 
+        //  Set up the player's event listener. Whenever they are notified
+        //  that another player took their turn, send them the new game
+        //  state
         newPlayer.eventListener.on('new-turn', (mark: Mark) => {
             socket.emit(
                 'board-update',
@@ -66,11 +69,11 @@ io.on('connection', (socket) => {
             );
         });
 
-        socketGameMap.set(socket.id, [newPlayer]);
+        socketPlayerMap.set(socket.id, [newPlayer]);
 
         //  Create a new session for the player and add the player to it
         const session = new Session();
-        sessionIdMap.set(newPlayer, session);
+        playerSessionMap.set(newPlayer, session);
         session.addPlayer(newPlayer);
 
         //  If player wanted a single player game, add an AI player to session
@@ -82,6 +85,7 @@ io.on('connection', (socket) => {
                 eventListener: new EventEmitter(),
             };
 
+            //  The AI's eventListener will trigger them to take their turn
             newAIPlayer.eventListener.on('new-turn', (mark: Mark) => {
                 if (mark === newAIPlayer.mark) {
                     let boardPosition = playerAIStrategy(session.getGame());
@@ -92,8 +96,11 @@ io.on('connection', (socket) => {
                 }
             });
 
+            //  Add the AI player and start the session
             session.addPlayer(newAIPlayer);
             session.start();
+
+            //  If game is a local multiplayer game, add another human player
         } else if (gameConfig.mode === 'localMultiplayer') {
             const newLocalPlayer = {
                 name: 'default-human-player-name',
@@ -102,119 +109,79 @@ io.on('connection', (socket) => {
                 eventListener: new EventEmitter(),
             };
 
-            const currentPlayer = socketGameMap.get(socket.id);
+            //  Add this human player to the socketPlayerMap
+            const currentPlayer = socketPlayerMap.get(socket.id);
             if (currentPlayer) {
-                socketGameMap.set(socket.id, [
+                socketPlayerMap.set(socket.id, [
                     currentPlayer[0],
                     newLocalPlayer,
                 ]);
                 session.addPlayer(newLocalPlayer);
-            }
-        }
-
-        /*
-        try 
-            const game = createGame(gameConfig);
-            if (game) {
-                console.log(`Game created with id ${game.getId()}`);
-
-                // TODO: Allow players to set names
-                const player = {
-                    name: 'default-player',
-                    mark: gameConfig.mark,
-                    gameId: game.getId(),
-                };
-                socketGameMap.set(socket.id, player);
-                gameIdMap.set(game.getId(), game);
-
-                socket.emit(
-                    'board-update',
-                    game.getBoard(),
-                    game.getPlayerTurn()
+            } else {
+                throw Error(
+                    "Couldn't find first player while adding second player"
                 );
             }
-        } catch (error) {
-            console.log(`Error starting game: ${error}`);
         }
-        */
     });
 
     socket.on('gameboard-click', (position) => {
-        // Find the session the player is connected to
-        const players = socketGameMap.get(socket.id);
+        //  Find the session the player is connected to
+        const players = socketPlayerMap.get(socket.id);
         if (players) {
             const player = players[0];
-            const session = sessionIdMap.get(player);
+            const session = playerSessionMap.get(player);
 
-            if (players.length === 1) {
-                if (session) {
-                    session.takeTurn(player, {
-                        row: position[0],
-                        col: position[1],
-                    });
-                    socket.emit(
-                        'board-update',
-                        session.getGame().getBoard(),
-                        session.turnHandler.currentMarkTurn
-                    );
-                }
-            } else if (players.length === 2) {
-                if (session) {
-                    const playerTurn =
-                        player.mark === session?.turnHandler.currentMarkTurn
-                            ? player
-                            : players[1];
-                    console.log('playerTurn = ', playerTurn);
-                    session.takeTurn(playerTurn, {
-                        row: position[0],
-                        col: position[1],
-                    });
-
-                    socket.emit(
-                        'board-update',
-                        session.getGame().getBoard(),
-                        session.turnHandler.currentMarkTurn
-                    );
-                }
+            if (!session) {
+                throw new Error('Error finding player session');
             }
-        }
 
-        /*
-        console.log(`\n${socket.id} clicked game board at ${position}`);
-
-        // Find game the player is connected to
-        const player = socketGameMap.get(socket.id);
-        if (player) {
-            const game = gameIdMap.get(player.gameId);
-            if (game) {
-                const turnResult = game.takeTurn(
-                    position[0],
-                    position[1],
-                    player.mark
+            //  If the map has two players for a single socket, then it is a
+            //  single player game. The player should simply be allowed to
+            //  take their turn
+            if (players.length === 1) {
+                session.takeTurn(player, {
+                    row: position[0],
+                    col: position[1],
+                });
+                socket.emit(
+                    'board-update',
+                    session.getGame().getBoard(),
+                    session.turnHandler.currentMarkTurn
                 );
+
+                //  If there are two players for a single socket, it is a local
+                //  multiplayer game and the turn should be taken by whichever
+                //  player is supposed to be going next
+            } else if (players.length === 2) {
+                const playerTurn =
+                    player.mark === session?.turnHandler.currentMarkTurn
+                        ? player
+                        : players[1];
+                session.takeTurn(playerTurn, {
+                    row: position[0],
+                    col: position[1],
+                });
 
                 socket.emit(
                     'board-update',
-                    game.getBoard(),
-                    game.getPlayerTurn()
+                    session.getGame().getBoard(),
+                    session.turnHandler.currentMarkTurn
                 );
+            }
+        } else {
+            socket.emit('invalid-player');
+            throw Error('Error finding players for session');
+        }
+    });
 
-
+    /*
                 if (turnResult === 1 || turnResult === 2 || turnResult === 3) {
                     const victoryPosition = game.getVictoryPosition();
                     socket.emit('game-won', turnResult, victoryPosition);
                 }
-            } else {
-                console.log("Unable to locate player's game");
-                // TODO: Send error back to client
-            }
-        } else {
-            console.log('Unable to locate player');
-            // TODO: Send error back to client
-            socket.emit('invalid-player');
-        }
-        */
-    });
+            
+    
 
     /*
     socket.on('reset-board', () => {
